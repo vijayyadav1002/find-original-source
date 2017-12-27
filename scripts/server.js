8af1734800317622b7a28c1aeeaf95b64fd75fe5
Vijay Yadav <vijay.yadav@sabre.com>
2017-12-27 09:57:58 -0600

    testing express server

--- scripts/server.js
+++ scripts/server.js
@@ -3,12 +3,14 @@ const app = require('express')();
 // const {static} = require('express');
 const buildPath = appRoot.resolve('/build');
 
+const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000;
+const HOST = process.env.HOST || '0.0.0.0';
 // app.use(static(buildPath));
 
 app.get('/', (req, res) => {
     res.json({status: 'ok'});
 });
-app.listen(3000, (err) => {
+app.listen(DEFAULT_PORT, HOST, (err) => {
     if (err) {
         console.error('Whoops!! something went wrong', err);
         return;
